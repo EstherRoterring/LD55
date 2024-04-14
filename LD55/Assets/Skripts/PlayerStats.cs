e6a8c895003d9732d75f63260fb042c775a3bb1e
tim-boe <111371051+tim-boe@users.noreply.github.com>
2024-04-14 16:36:40 +0200

    junge der manatrank funktioniert

--- LD55/Assets/Skripts/PlayerStats.cs
+++ LD55/Assets/Skripts/PlayerStats.cs
@@ -10,12 +10,21 @@ public class playerStats : MonoBehaviour
     public static int CurrentMana = 100;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "manaPotion")
+        {
+            CurrentMana = MaxMana;
+            collision.collider.gameObject.SetActive(false);
+        }
     }
 }
+    
